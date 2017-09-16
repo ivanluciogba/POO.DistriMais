8216686059e0a8dfd16a02fc016821ec24898cdc
ivanluciogba <31658363+ivanluciogba@users.noreply.github.com>
2017-09-16 00:14:45 -0300

    Add files via upload

---
+++ ClienteExistenteException.java
@@ -0,0 +1,12 @@
+
+public class ClienteExistenteException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	public ClienteExistenteException(String msgErro){
+		super(msgErro);
+	}
+}
